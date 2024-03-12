9efabd0dede5a6418da853b6708b949665e6d4f0
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-03-12 16:18:27 +0800

    initial

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-run-behavior
+//
+//  Created by Leo Wirasanto Laia on 12/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_run_behaviorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_run_behaviorApp.swift
+//  swiftui-run-behavior
+//
+//  Created by Leo Wirasanto Laia on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_run_behaviorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
