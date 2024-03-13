9ff2bed3d8f2aa142be3d6f9a9ade151bd005835
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-03-13 09:59:43 +0800

    feature: - add navigation to permission screen

--- Core/Dashboard/DashboardView.swift
+++ Core/Dashboard/DashboardView.swift
@@ -7,63 +7,69 @@
 
 import SwiftUI
 
+struct RouteDestination {
+    static let permissionScreen = "permission_screen"
+    static let runScreen = "run_screen"
+    static let formScreen = "form_screen"
+}
+
 struct DashboardView: View {
+    @State private var navigationPath = NavigationPath()
+    
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Hello!")
-                        .font(.subheadline)
-                        .foregroundStyle(.white)
-                    Text("Leo Laia")
-                        .foregroundStyle(.white)
-                        .font(.headline)
-                        .fontWeight(.semibold)
+        NavigationStack(path: $navigationPath) {
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Hello!")
+                            .font(.subheadline)
+                            .foregroundStyle(.white)
+                        Text("Leo Laia")
+                            .foregroundStyle(.white)
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                    }
+                    
+                    Spacer()
+                    
+                    Image("leo-pp")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 65, height: 65)
+                        .clipShape(Circle())
                 }
+                .padding()
+                
+                TabView {
+                    RunStatisticCarouselItem()
+                        .padding()
+                    
+                    FillFormCarouselItem()
+                        .padding()
+                }
+                .tabViewStyle(.page)
+                .frame(height: 550)
                 
                 Spacer()
                 
-                Image("leo-pp")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 65, height: 65)
-                    .clipShape(Circle())
-            }
-            .padding()
-            
-            TabView {
-                RunStatisticCarouselItem()
+                NavigationLink(destination: PermissionScreen()) {
+                    HStack(alignment: .bottom, spacing: 8) {
+                        Image(systemName: "figure.run")
+                            .foregroundStyle(.white)
+                        
+                        Text("Run")
+                            .font(.subheadline)
+                            .foregroundStyle(.white)
+                            .fontWeight(.semibold)
+                    }
                     .padding()
-                
-                FillFormCarouselItem()
-                    .padding()
-            }
-            .tabViewStyle(.page)
-            .frame(height: 550)
-            
-            Spacer()
-            
-            Button {
-                
-            } label: {
-                HStack(alignment: .bottom, spacing: 8) {
-                    Image(systemName: "figure.run")
-                        .foregroundStyle(.white)
-                    
-                    Text("Run")
-                        .font(.subheadline)
-                        .foregroundStyle(.white)
-                        .fontWeight(.semibold)
+                    .background(Color.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
-                .padding()
-                .background(Color.green)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
             }
+            .frame(maxHeight: .infinity)
+            .background(Color.black)
         }
-        .frame(maxHeight: .infinity)
-        .background(Color.black)
-        
-        
     }
 }
 
